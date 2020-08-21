5e393a31ac5cd732b53bacd4d4017b182bba158e
Mattia Zaratin <mattia.zaratin@larus-ba.it>
2020-08-21 12:24:58 +0200

    Updates

---
+++ precache-manifest.f3cd2a197160bb404d15e79f14f657f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85093f7f6a423bd936eab12754c4e201",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c10ba623d2e78a43470f",
+    "url": "./static/css/main.9beafbd8.chunk.css"
+  },
+  {
+    "revision": "14798837711741afac26",
+    "url": "./static/js/2.95da89d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.95da89d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10ba623d2e78a43470f",
+    "url": "./static/js/main.100f55b8.chunk.js"
+  },
+  {
+    "revision": "a9670dbbffb8e44dc4dc",
+    "url": "./static/js/runtime-main.0a9e78e1.js"
+  }
+]);
