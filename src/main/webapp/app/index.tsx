326a19734c6c3b8352af1c70dc0e8817625ce3e8
Mattia Zaratin <mattia.zaratin@larus-ba.it>
2020-08-21 17:49:08 +0200

    fix

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -11,6 +11,8 @@ import ErrorBoundary from './shared/error/error-boundary';
 import AppComponent from './app';
 import { loadIcons } from './config/icon-loader';
 
+import { CreateElement } from 'create-component';
+
 const devTools = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
 
 const store = initStore();
@@ -22,6 +24,35 @@ loadIcons();
 
 const rootEl = document.getElementById('root');
 
+const componentToCreate = {
+  align: 'vertical',
+  element: [
+    {
+      type: 'input',
+      text: '',
+      id: 'username',
+      name: 'name1',
+      mandatory: true,
+    },
+    {
+      type: 'password',
+      text: '',
+      id: 'password',
+      name: 'name3',
+      mandatory: true,
+    },
+    {
+      type: 'checkbox',
+      text: 'Remember me',
+      id: 'remember',
+      name: 'name4',
+      mandatory: true,
+    },
+  ],
+};
+
+const handleElement = (event: any, id: string) => {};
+
 const render = Component =>
   // eslint-disable-next-line react/no-render-return-value
   ReactDOM.render(
@@ -31,6 +62,7 @@ const render = Component =>
           {/* If this slows down the app in dev disable it and enable when required  */}
           {devTools}
           <Component />
+          <CreateElement componentToCreate={componentToCreate} onChangeElement={handleElement.bind(this)} />
         </div>
       </Provider>
     </ErrorBoundary>,
